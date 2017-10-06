a220a4beac34f4fbbf7d63ca0e0b9c58e0fa9361
Cristiano Faustino <cristiadu@gmail.com>
2017-10-06 12:48:39 -0300

    Including support for emoji tag

---
+++ src/main/java/org/symphonyoss/symphony/messageml/elements/Emoji.java
@@ -0,0 +1,77 @@
+package org.symphonyoss.symphony.messageml.elements;
+
+import org.symphonyoss.symphony.messageml.exceptions.InvalidInputException;
+import org.symphonyoss.symphony.messageml.util.XmlPrintStream;
+import org.w3c.dom.Node;
+
+public class Emoji extends Entity {
+
+  public static final String MESSAGEML_TAG = "emoji";
+  private static final String ATTR_NAME = "name";
+  private static final String TEXT_PREFIX = ":";
+  public static final String ENTITY_TYPE = "org.symphonyoss.emoji";
+  public static final String SUB_ENTITY_TYPE = "org.symphonyoss.emoji.name";
+  private static final String ENTITY_VERSION = "1.0";
+  private static final String ENTITY_ID_PREFIX = "emoji";
+
+  private String name;
+
+  public Emoji(Element parent, int entityIndex) {
+    super(parent, MESSAGEML_TAG, DEFAULT_PRESENTATIONML_TAG, FormatEnum.MESSAGEML);
+    this.entityId = getEntityId(entityIndex);
+  }
+
+  @Override
+  public void asPresentationML(XmlPrintStream out) {
+    out.printElement(presentationMLTag, asText(), CLASS_ATTR, Entity.PRESENTATIONML_CLASS, ENTITY_ID_ATTR, entityId);
+  }
+
+  @Override
+  public String asText() {
+    return TEXT_PREFIX + this.name + TEXT_PREFIX;
+  }
+
+  @Override
+  protected void buildAttribute(Node item) throws InvalidInputException {
+    switch (item.getNodeName()) {
+      case ATTR_NAME:
+        this.name = getStringAttribute(item);
+        break;
+      default:
+        throw new InvalidInputException("Attribute \"" + item.getNodeName() + "\" is not allowed in \"" + getMessageMLTag() + "\"");
+    }
+  }
+
+  @Override
+  public void validate() throws InvalidInputException {
+    if (this.name==null) {
+      throw new InvalidInputException("The attribute \"name\" is required");
+    }
+  }
+
+
+  @Override
+  protected String getEntityValue() {
+    return name;
+  }
+
+  @Override
+  protected String getEntitySubType() {
+    return SUB_ENTITY_TYPE;
+  }
+
+  @Override
+  protected String getEntityVersion() {
+    return ENTITY_VERSION;
+  }
+
+  @Override
+  protected String getEntityType() {
+    return ENTITY_TYPE;
+  }
+
+  @Override
+  protected String getEntityIdPrefix() {
+    return ENTITY_ID_PREFIX;
+  }
+}
