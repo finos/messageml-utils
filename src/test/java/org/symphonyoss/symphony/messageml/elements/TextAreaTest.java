e264909781c6c19cd7c3e4915ad731b9c6ed2999
Sandro Ribeiro <36516402+sribeiro-daitan@users.noreply.github.com>
2019-06-14 11:10:56 -0300

    MML-112 - Add the Symphony Element 'textarea' tag (#113)
    * MML-112 - Add the Symphony Element 'textarea' tag
    
    * MML-112 - Addressing most of the PR comments
    
    * MML-112 - Addressing additional PR comments
    
    * MML-112 - Fix typo
    
    * MML-112 - removed unnecessary method parameters
    
    * MML-112 - update constructor call and remove no longer necessary override after #111 merge
    
    * MML-112 - Moved current Markdown tests to inside each necessary test method
    
    * MML-112 - Moved current expected markdown output to static variables; Added getText() tests
    
    * MML-112 - Moved current PresentationML tests to inside each necessary test method, plus some optimizations
    
    * MML-112 - form id is now required after rebasing latest commits

---
+++ src/test/java/org/symphonyoss/symphony/messageml/elements/TextAreaTest.java
@@ -0,0 +1,164 @@
+package org.symphonyoss.symphony.messageml.elements;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.symphonyoss.symphony.messageml.exceptions.InvalidInputException;
+
+public class TextAreaTest extends ElementTest {
+
+  private static final String NAME_VALUE = "A name";
+  private static final String PLACEHOLDER_VALUE = "A placeholder";
+  private static final String INITIAL_VALUE = "An initial value";
+
+  private static final String EXPECTED_MARKDOWN = "Form (log into desktop client to answer):\n---\n(Text Area)\n\n---\n";
+  private static final String EXPECTED_MARKDOWN_WITH_PLACEHOLDER =
+      String.format("Form (log into desktop client to answer):\n---\n(Text Area:%s)\n\n---\n", PLACEHOLDER_VALUE);
+
+  @Test
+  public void testTextAreaWithRequiredAttributesOnly() throws Exception {
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\"></textarea></form></messageML>", NAME_VALUE);
+    String expectedPresentationML =
+        String.format("<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\"></textarea></form></div>",
+            NAME_VALUE);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertTrue("Text should be empty", context.getText().isEmpty());
+  }
+
+  @Test
+  public void testTextAreaWithAllAttributes() throws Exception {
+    String input =
+        String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\" placeholder=\"%s\" required=\"true\">%s</textarea></form></messageML>",
+            NAME_VALUE, PLACEHOLDER_VALUE, INITIAL_VALUE);
+    String expectedPresentationML = String.format(
+        "<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\" placeholder=\"%s\" "
+            + "required=\"true\">%s</textarea></form></div>",
+        NAME_VALUE, PLACEHOLDER_VALUE, INITIAL_VALUE);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN_WITH_PLACEHOLDER, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertEquals("Text should be the initial value", INITIAL_VALUE, context.getText());
+  }
+
+  @Test
+  public void testTextAreaWithInitialValue() throws Exception {
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\">%s</textarea></form></messageML>", NAME_VALUE, INITIAL_VALUE);
+    String expectedPresentationML =
+        String.format("<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\">%s</textarea></form></div>",
+            NAME_VALUE, INITIAL_VALUE);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertEquals("Text should be the initial value", INITIAL_VALUE, context.getText());
+  }
+
+  @Test
+  public void testTextAreaRequiredAttribute() throws Exception {
+    String input =
+        String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\" required=\"true\"></textarea></form></messageML>", NAME_VALUE);
+    String expectedPresentationML = String.format(
+        "<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\" required=\"true\"></textarea></form></div>",
+        NAME_VALUE);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertTrue("Text should be empty", context.getText().isEmpty());
+  }
+
+  @Test
+  public void testTextAreaPlaceholderAttribute() throws Exception {
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\" placeholder=\"%s\"></textarea></form></messageML>", NAME_VALUE,
+        PLACEHOLDER_VALUE);
+    String expectedPresentationML = String.format(
+        "<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\" placeholder=\"%s\"></textarea></form></div>",
+        NAME_VALUE, PLACEHOLDER_VALUE);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN_WITH_PLACEHOLDER, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertTrue("Text should be empty", context.getText().isEmpty());
+  }
+
+  @Test
+  public void testTextAreaWithoutRequiredFields() throws Exception {
+    String input = "<messageML><form id=\"form-id\"><textarea></textarea></form></messageML>";
+
+    expectedException.expect(InvalidInputException.class);
+    expectedException.expectMessage("The attribute \"name\" is required");
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+  }
+
+  @Test
+  public void testTextAreaWithInvalidRequiredAttributeValue() throws Exception {
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\" required=\"value\"></textarea></form></messageML>", NAME_VALUE);
+
+    expectedException.expect(InvalidInputException.class);
+    expectedException.expectMessage(
+        "Attribute \"required\" of element \"textarea\" can only be one of the following values: [true, false].");
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+  }
+
+  @Test
+  public void testTextAreaWithNotAllowedAttribute() throws Exception {
+    String attribute = "anotherAttribute";
+    String input = String.format("<messageML><form id=\"form-id\"><textarea %s=\"value\"></textarea></form></messageML>", attribute);
+
+    expectedException.expect(InvalidInputException.class);
+    expectedException.expectMessage("Attribute \"" + attribute + "\" is not allowed in \"textarea\"");
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+  }
+
+  @Test
+  public void testTextAreaWithChildren() throws Exception {
+    String childElement = "div";
+    String initialValueWithChildren = String.format("<%s>%s</%s>", childElement, INITIAL_VALUE, childElement);
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\">%s</textarea></form></messageML>", NAME_VALUE,
+        initialValueWithChildren);
+
+    expectedException.expect(InvalidInputException.class);
+    expectedException.expectMessage("Element \"" + childElement + "\" is not allowed in \"textarea\"");
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+  }
+
+}
