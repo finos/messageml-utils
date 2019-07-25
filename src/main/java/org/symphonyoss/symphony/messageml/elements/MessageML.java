f47211f27221ed379e50ac2a0eb036b38e673693
Sandro Ribeiro <sribeiro@daitan.com>
2019-07-25 14:17:07 -0300

    Revert "PLAT-8025 - removed unused imports"
    This reverts commit e43f902d8f25885e6155998f7e1ef53f10be0276.

--- src/main/java/org/symphonyoss/symphony/messageml/elements/MessageML.java
+++ src/main/java/org/symphonyoss/symphony/messageml/elements/MessageML.java
@@ -28,9 +28,12 @@ import org.commonmark.node.Document;
 import org.symphonyoss.symphony.messageml.exceptions.InvalidInputException;
 import org.symphonyoss.symphony.messageml.util.XmlPrintStream;
 
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 /**
