468cd61109fcf5de290d3af310cacbbfd0c027e2
Sandro Ribeiro <sribeiro@daitan.com>
2019-08-01 18:37:06 -0300

    PLAT-8050 - moving max count value to the caller

--- src/main/java/org/symphonyoss/symphony/messageml/elements/Element.java
+++ src/main/java/org/symphonyoss/symphony/messageml/elements/Element.java
@@ -56,7 +56,6 @@ public abstract class Element {
   public static final String CLASS_ATTR = "class";
   public static final String STYLE_ATTR = "style";
   private static final String ID_ATTR = "id";
-  private static final int MAX_COUNT_PER_CHILD_TYPE = 20;
 
   protected FormatEnum format;
   private final Map<String, String> attributes = new LinkedHashMap<>();
@@ -456,11 +455,11 @@ public abstract class Element {
    * @param elementTypes The element types that will be verified in order to ensure that the maximum count was not exceed.
    * @throws InvalidInputException
    */
-  void assertChildrenNotExceedingMaxCount(Collection<Class<? extends Element>> elementTypes) throws InvalidInputException {
-    boolean hasExceeded = elementTypes.stream().anyMatch(type -> findElements(type).size() > MAX_COUNT_PER_CHILD_TYPE);
+  void assertChildrenNotExceedingMaxCount(Collection<Class<? extends Element>> elementTypes, int maxCountPerElementType) throws InvalidInputException {
+    boolean hasExceeded = elementTypes.stream().anyMatch(type -> findElements(type).size() > maxCountPerElementType);
     if (hasExceeded) {
       throw new InvalidInputException(String.format("Element \"form\" cannot have more than %s children of the following elements: [%s].",
-          MAX_COUNT_PER_CHILD_TYPE, getElementsNameByClassName(elementTypes)));
+          maxCountPerElementType, getElementsNameByClassName(elementTypes)));
     }
   }
 
