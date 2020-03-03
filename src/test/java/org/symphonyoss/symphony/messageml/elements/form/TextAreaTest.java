e37d3109a041aceb9ed1ba89a818f60c443fd3eb
Your Name <you@example.com>
2020-03-03 08:47:14 -0400

    allowed line break in textarea

--- src/test/java/org/symphonyoss/symphony/messageml/elements/form/TextAreaTest.java
+++ src/test/java/org/symphonyoss/symphony/messageml/elements/form/TextAreaTest.java
@@ -16,6 +16,7 @@ public class TextAreaTest extends ElementTest {
   private static final String NAME_VALUE = "A name";
   private static final String PLACEHOLDER_VALUE = "A placeholder";
   private static final String INITIAL_VALUE = "An initial value";
+  private static final String LINE_BREAK = "An \n initial \n value";
 
   private static final String EXPECTED_MARKDOWN = "Form (log into desktop client to answer):\n---\n(Text Area)" + ACTION_BTN_MARKDOWN
       + "\n---\n";
@@ -23,6 +24,8 @@ public class TextAreaTest extends ElementTest {
       String.format("Form (log into desktop client to answer):\n---\n(Text Area:[%s])" + ACTION_BTN_MARKDOWN + "\n---\n", PLACEHOLDER_VALUE);
   private static final String EXPECTED_MARKDOWN_WITH_INITIAL_VALUE =
       String.format("Form (log into desktop client to answer):\n---\n(Text Area:%s)" + ACTION_BTN_MARKDOWN + "\n---\n", INITIAL_VALUE);
+  private static final String EXPECTED_MARKDOWN_WITH_LINE_BREAK =
+      String.format("Form (log into desktop client to answer):\n---\n(Text Area:%s)" + ACTION_BTN_MARKDOWN + "\n---\n", LINE_BREAK);
   private static final String EXPECTED_MARKDOWN_WITH_PLACEHOLDER_AND_INITIAL_VALUE =
       String.format("Form (log into desktop client to answer):\n---\n(Text Area:[%s]%s)" + ACTION_BTN_MARKDOWN + "\n---\n", PLACEHOLDER_VALUE, INITIAL_VALUE);
   
@@ -86,6 +89,27 @@ public class TextAreaTest extends ElementTest {
     assertEquals("Markdown", EXPECTED_MARKDOWN_WITH_INITIAL_VALUE, context.getMarkdown());
     assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
     assertEquals("Text should be the initial value", INITIAL_VALUE, textArea.getChild(0).asText());
+  }
+
+
+  @Test
+  public void testTextAreaWithLineBreak() throws Exception {
+    String input = String.format("<messageML><form id=\"form-id\"><textarea name=\"%s\">%s</textarea>%s</form></messageML>", NAME_VALUE, LINE_BREAK,
+            ACTION_BTN_ELEMENT);
+    String expectedPresentationML =
+            String.format("<div data-format=\"PresentationML\" data-version=\"2.0\"><form id=\"form-id\"><textarea name=\"%s\">%s</textarea>%s</form></div>",
+                    NAME_VALUE, LINE_BREAK, ACTION_BTN_ELEMENT);
+
+    context.parseMessageML(input, null, MessageML.MESSAGEML_VERSION);
+    Element messageML = context.getMessageML();
+    Element form = messageML.getChildren().get(0);
+    Element textArea = form.getChildren().get(0);
+
+    assertEquals(Form.class, form.getClass());
+    assertEquals(TextArea.class, textArea.getClass());
+    assertEquals("Markdown", EXPECTED_MARKDOWN_WITH_LINE_BREAK, context.getMarkdown());
+    assertEquals("PresentationML", expectedPresentationML, context.getPresentationML());
+    assertEquals("Text should be the initial value", LINE_BREAK, textArea.getChild(0).asText());
   }
 
   @Test
