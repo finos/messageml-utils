be38a7fb5ff89d98c53615bf1c9255216966ff57
Lucas <lmacedo@daitangroup.com>
2020-07-29 19:09:50 -0300

    Logging information that we are removing the "-destructive" from the class

--- src/main/java/org/symphonyoss/symphony/messageml/elements/Button.java
+++ src/main/java/org/symphonyoss/symphony/messageml/elements/Button.java
@@ -1,6 +1,8 @@
 package org.symphonyoss.symphony.messageml.elements;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.symphonyoss.symphony.messageml.MessageMLContext;
 import org.symphonyoss.symphony.messageml.MessageMLParser;
 import org.symphonyoss.symphony.messageml.exceptions.InvalidInputException;
@@ -23,6 +25,8 @@ import java.util.Set;
  * @since 03/21/19
  */
 public class Button extends FormElement {
+  
+  Logger logger = LoggerFactory.getLogger(Button.class);
 
   public static final String MESSAGEML_TAG = "button";
   public static final String ACTION_TYPE = "action";
@@ -47,14 +51,10 @@ public class Button extends FormElement {
         break;
         // The button can a have tooltips but is not a tooltipable element because it dont generate the span with tooltip
       case CLASS_ATTR:
-        setAttribute(item.getNodeName(), getStringAttribute(item));
-        String clazz = getAttribute(CLASS_ATTR);
-        if("primary-destructive".equals(clazz)) {
-          setAttribute(item.getNodeName(), "primary");
+        if(getStringAttribute(item).contains("-destructive")) {
+          logger.info("Button class cannot be a destructive one, replacing it accordingly.");
         }
-        if("secondary-destructive".equals(clazz)) {
-          setAttribute(item.getNodeName(), "secondary");
-        }
+        setAttribute(item.getNodeName(), StringUtils.removeEnd(getStringAttribute(item), "-destructive"));
         break;
       case TooltipableElement.TITLE:
         if(format != FormatEnum.MESSAGEML){
