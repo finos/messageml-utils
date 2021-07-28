47947a10b8424eea406f6e2a72ef890f6523bb71
symphony-mariacristina <maria.dominicis@symphony.com>
2021-07-28 17:34:27 +0200

    Preloading the messageML and entityJson file

--- src/test/java/org/symphonyoss/symphony/messageml/MessageMLContextBenchmark.java
+++ src/test/java/org/symphonyoss/symphony/messageml/MessageMLContextBenchmark.java
@@ -2,6 +2,10 @@ package org.symphonyoss.symphony.messageml;
 
 import org.apache.commons.io.IOUtils;
 import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.infra.Blackhole;
 import org.symphonyoss.symphony.messageml.exceptions.InvalidInputException;
 import org.symphonyoss.symphony.messageml.exceptions.ProcessingException;
@@ -12,6 +16,21 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class MessageMLContextBenchmark {
+
+  @State(Scope.Thread)
+  public static class MessageContent {
+    public String messageML;
+    public String entityJson;
+
+    @Setup(Level.Trial)
+    public void doSetup() throws IOException {
+      FileInputStream messageFile =
+          new FileInputStream("src/test/resources/payloads/complex_message_with_styles.messageml");
+      messageML = IOUtils.toString(messageFile, StandardCharsets.UTF_8);
+      FileInputStream entityFile = new FileInputStream("src/test/resources/payloads/complex_message_with_styles.json");
+      entityJson = IOUtils.toString(entityFile, StandardCharsets.UTF_8);
+    }
+  }
 
   @Benchmark
   public void parseSimpleMessageML(Blackhole bh) throws InvalidInputException, ProcessingException, IOException {
@@ -24,15 +43,10 @@ public class MessageMLContextBenchmark {
   }
 
   @Benchmark
-  public void parseComplexMessageMLWithEntities(Blackhole bh) throws InvalidInputException, ProcessingException, IOException {
+  public void parseComplexMessageMLWithEntities(MessageContent messageContent, Blackhole bh)
+      throws InvalidInputException, ProcessingException, IOException {
     MessageMLContext messageMLContext = new MessageMLContext(new NoOpDataProvider());
-
-    FileInputStream messageFile =
-        new FileInputStream("src/test/resources/payloads/complex_message_with_styles.messageml");
-    String messageML = IOUtils.toString(messageFile, StandardCharsets.UTF_8);
-    FileInputStream entityFile = new FileInputStream("src/test/resources/payloads/complex_message_with_styles.json");
-    String entityJson = IOUtils.toString(entityFile, StandardCharsets.UTF_8);
-    messageMLContext.parseMessageML(messageML, entityJson, null);
+    messageMLContext.parseMessageML(messageContent.messageML, messageContent.entityJson, null);
 
     // those calls are usually made by the agent when sending a message
     bh.consume(messageMLContext.getText());
